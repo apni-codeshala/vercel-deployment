1805d7d3fee8a394d1b1a4050d39d4e1b17af27c
apni-codeshala <abhishekpatel171019@gmail.com>
2024-02-28 18:53:30 +0000

    some changes

--- server/src/app.js
+++ server/src/app.js
@@ -4,12 +4,12 @@ import cookieParser from 'cookie-parser'
 
 const app=express()
 
-// app.use(cors({
-//     origin:process.env.CORS_ORIGIN,
-//     credentials:true,
-// }))
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
 
-app.use(cors());
+// app.use(cors());
 
 //middlerwares
 app.use(express.json({limit:"10kb"}))
