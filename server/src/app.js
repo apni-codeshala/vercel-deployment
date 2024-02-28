f319a2ed6dde3c6f0f4f2bb8e0aad6f6e2813348
apni-codeshala <abhishekpatel171019@gmail.com>
2024-02-28 18:57:36 +0000

    some changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,27 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-const app=express()
+const app = express();
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true,
-}))
+const corsOptions = {
+  origin: ['https://airbnb-saswat.vercel.app'], // Change this to your frontend URL
+  credentials: true,
+};
 
-// app.use(cors());
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 //middlerwares
-app.use(express.json({limit:"10kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))     //public assets for storing files
-app.use(cookieParser())
-
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public')); // public assets for storing files
+app.use(cookieParser());
 
 //routes importing
-import userRouter from './routes/user.routes.js'
-
-
+import userRouter from './routes/user.routes.js';
 
 //routes declaration
-app.use("/api/v1/users", userRouter)
-//http://localhost:8000/api/v1/users/register
+app.use('/api/v1/users', userRouter); // http://localhost:8000/api/v1/users/register
 
-export {app}
+export { app };
